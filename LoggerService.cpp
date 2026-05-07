47b834821b65c4b47166f06a1d81dca6ce20b6b1
Kelvin Fong <177859288+kelvinfongkm@users.noreply.github.com>
2026-05-07 20:44:26 +0800

    Adds LogRecord struct

--- LoggerService.cpp
+++ LoggerService.cpp
@@ -1,11 +1,18 @@
 ﻿#include "LoggerService.h"
-#include <format>
+#include <chrono>
 #include <iostream>
 #include <string>
 
+static const std::chrono::time_zone* timezone = std::chrono::current_zone();
+
+struct LogRecord
+{
+	std::chrono::system_clock::time_point timestamp;
+	std::string message;
+};
+
 typedef struct logger_t
 {
-
 } logger_t;
 
 logger_t* logger_create()
@@ -22,6 +29,15 @@ void logger_destroy(logger_t* logger)
 void logger_log(logger_t* logger, const char* message, size_t len)
 {
 	if (logger == NULL) return;
-	std::string str(message, len);
-	std::clog << std::format("{}\n", str);
+
+	LogRecord record;
+
+	record.timestamp = std::chrono::system_clock::now();
+
+	if (message != NULL) record.message.assign(message, len);
+
+	std::clog << std::chrono::zoned_time(timezone, record.timestamp)
+		<< '\t'
+		<< record.message
+		<< '\n';
 }
